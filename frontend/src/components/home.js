09bf3c5c097edba887c3d75b391c3b6b459bd2d7
Denise Boler <sts127@txstate.edu>
2025-02-19 21:20:43 -0600

    SCRUM-81
    general home and pantry page setup

---
+++ frontend/src/components/home.js
@@ -0,0 +1,8 @@
+const Home = () => (
+    <div className="home">
+      <h2>Welcome to Recipe Generator </h2>
+      <p> this is the home page</p>
+    </div>
+  );
+  
+  export default Home;  
---
+++ frontend/src/components/pantry.js
@@ -0,0 +1,9 @@
+const Pantry = () => (
+    <div className="pantry">
+      <h2>Pantry Inventory</h2>
+      <p>Start building your UI here.</p>
+    </div>
+  );
+  
+  export default Pantry;
+  
