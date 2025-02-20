edd5497eb589e95dcb7332f2022cc37e3f701325
Denise Boler <sts127@txstate.edu>
2025-02-20 17:55:48 -0600

    SCRUM-82
    implemented react-router-dom instead of useState for page navigation

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,8 +1,14 @@
-const Home = () => (
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
     <div className="home">
-      <h2>Welcome to Recipe Generator </h2>
-      <p> this is the home page</p>
+      <h2>Welcome to the Recipe Generator</h2>
+      <button onClick={() => navigate("/pantry")}>Go to Pantry</button>
     </div>
   );
-  
-  export default Home;  
+};
+
+export default Home;
