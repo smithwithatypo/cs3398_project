4c0387367a55318b0225749d0e2d54a467ac193e
elliot sonoqui <ens46@txstate.edu>
2025-02-20 14:46:55 -0600

    SCRUM-59 added text box and start button to homepage plus CSS with color

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,8 +1,21 @@
-const Home = () => (
-    <div className="home">
-      <h2>Welcome to Recipe Generator </h2>
-      <p> this is the home page</p>
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="how-it-works">
+        <h2>How it works:</h2>
+        <p>
+          Bacon ipsum dolor amet tri-tip cow capicola, strip steak prosciutto
+          andouille venison pork belly salami bacon jowl ham hock leberkas. Jowl
+          turducken boudin cupim ground round ball tip filet mignon frankfurter
+          beef t-bone.
+        </p>
+        <button className="start-button">Start Here!</button>
+      </div>
     </div>
   );
-  
-  export default Home;  
+};
+
+export default Home;
