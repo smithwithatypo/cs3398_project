7c62f11fc63247c645485aac1f83c2dfa39e53a6
elliot sonoqui <ens46@txstate.edu>
2025-02-21 16:34:00 -0600

    SCRUM-59 fixed homepage paragraph

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -16,10 +16,7 @@ const Home = () => {
       <section className="how-it-works">
         <h2>How it works:</h2>
         <p>
-          Bacon ipsum dolor amet tri-tip cow capicola, strip steak prosciutto
-          andouille venison pork belly salami bacon jowl ham hock leberkas. Jowl
-          turducken boudin cupim ground round ball tip filet mignon frankfurter
-          beef t-bone.
+        Welcome to Recipe Generator! Easily turn your ingredients into delicious meals with just a few taps. Enter the ingredients you have, and we’ll suggest recipes you can make. Ready to start cooking? Search for a recipe or add your ingredients to begin!
         </p>
         <button className="start-button" onClick={() => navigate("/pantry")}>
           Start Here!
