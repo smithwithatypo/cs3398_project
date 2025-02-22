bf9f12730f3392a9388267f2100b02d9fc69dfbe
Denise Boler <sts127@txstate.edu>
2025-02-22 03:25:10 -0600

    SCRUM-82
    add item to pantry

--- frontend/src/components/pantry.js
+++ frontend/src/components/pantry.js
@@ -2,10 +2,27 @@ import { useState } from "react";
 const Pantry = () => {
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
+
+  const addItem = () => {
+    if (newItem.trim() !== "") {
+      setItems([...items, newItem]);
+      setNewItem("");
+    }
+  };
+
   return(
     <div>
       <h2>Pantry</h2>
           <p>Add and manage your pantry items to generate recipes based on what you have.</p>
+      <h3>Pantry Inventory</h3>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter item"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)} />
+        <button onClick={addItem}>Add Item</button>
+        </div>
     </div>
   );
 };
