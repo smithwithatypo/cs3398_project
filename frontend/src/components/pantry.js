4391672d01df1eeaa5a8b728552e5efa6c1ee665
Denise Boler <sts127@txstate.edu>
2025-02-22 03:17:52 -0600

    SCRUM-82
    implement useState hooks for pantry inventory use

--- frontend/src/components/pantry.js
+++ frontend/src/components/pantry.js
@@ -1,9 +1,13 @@
-const Pantry = () => (
-    <div className="pantry">
-      <h2>Pantry Inventory</h2>
-      <p>Start building your UI here.</p>
+import { useState } from "react";
+const Pantry = () => {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  return(
+    <div>
+      <h2>Pantry</h2>
+          <p>Add and manage your pantry items to generate recipes based on what you have.</p>
     </div>
   );
-  
-  export default Pantry;
-  
+};
+
+export default Pantry;
