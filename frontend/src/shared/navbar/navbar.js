a0e193d713be17baa3a67890ac6f411aacda1c0d
Cameron Archuleta <cja106@txstate.edu>
2025-02-25 15:37:03 -0600

    SCRUM-72 Nav Bar Creation
    Created Nav Bar

--- frontend/src/shared/navbar/navbar.js
+++ frontend/src/shared/navbar/navbar.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "./styles.css"; // Ensure the CSS file is imported
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Recipe Generator</h1>
-      <ul>
+      <h1 className="logo">Recipe Generator</h1>
+      <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/pantry">Pantry</Link></li>
       </ul>
