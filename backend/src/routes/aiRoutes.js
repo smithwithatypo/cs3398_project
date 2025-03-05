7aa747dd837cb26635c9d2c03edf4f2f8720214b
Denise Boler <sts127@txstate.edu>
2025-03-05 11:22:35 -0600

    SCRUM-123
    adding pantry routes  to aiRoutes

--- backend/src/routes/aiRoutes.js
+++ backend/src/routes/aiRoutes.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { TestController } from '../controllers/testController.js';
 import { GenerateRecipeController } from '../controllers/generateRecipeController.js';
+import { PantryController } from '../controllers/pantryItemsController.js';
 
 const router = express.Router();
 
-// routes
+// Test routes
 router.get('/test', TestController.getTest);
 router.post('/generate-text', GenerateRecipeController.getGeneratedRecipe);
+
+// Pantry routes
+router.get('/api/pantry', PantryController.getPantryItems);
+router.post('/api/pantry', PantryController.addPantryItem);
+router.delete('/api/pantry/:index', PantryController.removePantryItem);
 
 export default router;
