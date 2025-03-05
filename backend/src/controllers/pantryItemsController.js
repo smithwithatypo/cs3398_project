9c5e01b85d6176d631ec0751b63ad7420d07067f
Denise Boler <sts127@txstate.edu>
2025-03-05 11:23:20 -0600

    SCRUM-123
    changed function syntax to match other existing controllers

--- backend/src/controllers/pantryItemsController.js
+++ backend/src/controllers/pantryItemsController.js
@@ -1,11 +1,15 @@
-const PantryController = require("../services/pantryItemsService");
-const getPantryItems = async (req, res) => {
-    // GET
-};
-const addPantryItem = async (req, res) => {
-    // POST
-};
-const removePantryItem = async (req, res) => {
-    // DELETE
-};
-module.exports = { getPantryItems, addPantryItem, removePantryItem };
+//const PantryController = require("../services/pantryItemsService");
+
+const PantryController = {
+    async getPantryItems(req, res){
+        // GET
+    },
+    async addPantryItem(req, res) {
+        // POST
+    },
+    async removePantryItem(req, res){
+        // DELETE
+    },
+}
+
+export {PantryController}; 
