315458903d68b83da3e9896fd71b537888ff104e
Denise Boler <sts127@txstate.edu>
2025-03-05 11:14:15 -0600

    SCRUM-123
    set up for defining pantry get, add, and delete routes

--- backend/src/controllers/pantryItemsController.js
+++ backend/src/controllers/pantryItemsController.js
@@ -1,1 +1,11 @@
-import { PantryService } from '../services/pantryService.js';
+const PantryController = require("../services/pantryItemsService");
+const getPantryItems = async (req, res) => {
+    // GET
+};
+const addPantryItem = async (req, res) => {
+    // POST
+};
+const removePantryItem = async (req, res) => {
+    // DELETE
+};
+module.exports = { getPantryItems, addPantryItem, removePantryItem };
