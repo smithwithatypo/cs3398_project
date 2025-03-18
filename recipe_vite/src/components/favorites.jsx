c001db187f267525fc0779d6c5e2d2f6c9511f0f
Cameron Archuleta <cja106@txstate.edu>
2025-03-18 13:59:49 -0500

    SCRUM-127 Created "Favorites" page

---
+++ recipe_vite/src/components/favorites.jsx
@@ -0,0 +1,10 @@
+const Favorites = () => {
+    return (
+      <div className="min-h-screen bg-white font-sans flex flex-col items-center justify-center p-10">
+        <h2 className="text-xl font-bold">Your Favorite Recipes</h2>
+        <p className="text-gray-600 mt-2">No favorite recipes yet.</p>
+      </div>
+    );
+  };
+  
+  export default Favorites;
