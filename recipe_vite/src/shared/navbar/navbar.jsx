8158a400bef134a59bf7b6ff44071a5dda0573c4
Cameron Archuleta <cja106@txstate.edu>
2025-03-24 23:33:50 -0500

    SCRUM-73 Added Icon to navbar
    Added the icon next to "Recipe Generator"

--- recipe_vite/src/shared/navbar/navbar.jsx
+++ recipe_vite/src/shared/navbar/navbar.jsx
@@ -4,7 +4,10 @@ import icon from "../../assets/icon.jpg";
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-[#114945] p-4">
-      <h1 className="text-white text-xl font-bold m-0">Recipe Generator</h1>
+      <div className="flex items-center gap-2">
+        <img src={icon} alt="Logo" className="h-10 w-auto rounded-full" />
+        <h1 className="text-white text-xl font-bold m-0">Recipe Generator</h1>
+      </div>
       <ul className="flex gap-6 m-0 p-0 list-none">
         <li>
           <Link
