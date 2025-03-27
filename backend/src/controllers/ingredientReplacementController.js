086a1b8b156901bc93b9e48af337f9c42c44d078
Cameron Archuleta <cja106@txstate.edu>
2025-03-27 15:20:46 -0500

    SCRUM-125 created ingredientReplacementController
    added necessary imports & exports and created controller object & getReplacements method

---
+++ backend/src/controllers/ingredientReplacementController.js
@@ -0,0 +1,8 @@
+import { IngredientReplacementService } from '../services/ingredientReplacementService.js';
+
+const IngredientReplacementController = {
+  async getReplacements(req, res) {
+  }
+};
+
+export { IngredientReplacementController };
