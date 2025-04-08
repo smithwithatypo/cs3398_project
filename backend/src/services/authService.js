d1fe2d40f71c6ca5156d3cbe5088f8452be86fd3
Amanda Orozco <lbp42@txstate.edu>
2025-04-07 22:21:25 -0500

    SCRUM-173
    Contains business logic for user authentication. It helps separate authentication logic from controller code for cleaner organization

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,23 +1,20 @@
-const AuthService = {
+import User from "../../database/models/User.js";
+import bcrypt from 'bcrypt';
+
+export const AuthService = {
     getTestMessage() {
         return "This is a test message from the AuthService";
     },
 
-    checkCredentials(clientData) {
-        const valid_login = { email: 'test@gmail.com', password: 'pw' }
-
-        // console.log("clientdata inside authService:", clientData)    // for debug
-        // console.log("valid_login inside authService:", valid_login)  // for debug
-
-        // if matches database, return true
-        if (clientData.email === valid_login.email &&
-            clientData.password === valid_login.password
-        ) {
-            return true
-        } else {
-            return false
+    async checkCredentials({ email, password }) {
+        // Find the user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+          return false; // No user exists with that email
         }
-    }
+    // Compare the provided password with the hashed password in the database.
+    const isMatch = await bcrypt.compare(password, user.password);
+    return isMatch;
+    },
 };
-
-export { AuthService };
+export default AuthService;
