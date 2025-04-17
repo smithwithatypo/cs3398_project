1620d62a71b957e95cd1ed355131ae855baa371b
Denise Boler <sts127@txstate.edu>
2025-04-17 00:33:28 -0500

    SCRUM-144
    update controller parameters to include image

--- backend/src/controllers/generateRecipeController.js
+++ backend/src/controllers/generateRecipeController.js
@@ -1,4 +1,4 @@
-import { RecipeGeneratingService } from '../services/generateRecipeService.js';
+import { RecipeGeneratingService, generateRecipeImage} from '../services/generateRecipeService.js';
 
 const GenerateRecipeController = {
     async getGeneratedRecipe(req, res) {
@@ -37,7 +37,9 @@ const GenerateRecipeController = {
             `;
             
             const response = await RecipeGeneratingService.generateRecipe(userPrompt);
-            res.status(200).json({ success: true, data: response });
+            const recipeImage = await generateRecipeImage(recipeText);
+
+            res.status(200).json({ success: true, data: {response, recipeImage} });
         } catch (error) {
             console.error('Error generating recipe from pantry items:', error);
             res.status(500).json({ 
@@ -70,7 +72,9 @@ const GenerateRecipeController = {
             `;
             
             const response = await RecipeGeneratingService.generateRecipeFromText(userPrompt);
-            res.status(200).json({ success: true, data: response });
+            const recipeImage = await generateRecipeImage(recipeText);
+
+            res.status(200).json({ success: true, data: {response, recipeImage} });
         } catch (error) {
             console.error('Error generating recipe from text:', error);
             res.status(500).json({ 
